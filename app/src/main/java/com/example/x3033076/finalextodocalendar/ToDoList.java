ed763c7075d8ddc8da49aa1e6423b050d042c85e
Keisuke <ji.kei1220@gmail.com>
2020-06-17 09:20:25 +0900

    ToDoリスト内のButtonとリストビューをリスナ登録し、 リストをクリックするとレイアウトのタイトル名にリストの名前が表示されるようにした。 レイアウトは微調整

--- app/src/main/java/com/example/x3033076/finalextodocalendar/ToDoList.java
+++ app/src/main/java/com/example/x3033076/finalextodocalendar/ToDoList.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,9 +17,13 @@ import androidx.fragment.app.Fragment;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ToDoList extends Fragment {
+public class ToDoList extends Fragment implements View.OnClickListener {
 
     ListView list;
+    Button finishButton, editButton, deleteButton;
+    TextView toDoTitle;
+
+    String getTitle = "";
 
     static List<String> dataList = new ArrayList<String>();
     static ArrayAdapter<String> adapter;
@@ -29,7 +36,17 @@ public class ToDoList extends Fragment {
                              @Nullable Bundle savedInstanceState) {
 
         tdRootView = inflater.inflate(R.layout.todo_list_layout, container, false);
+
         list = (ListView) tdRootView.findViewById(R.id.toDoLV);
+        toDoTitle = (TextView) tdRootView.findViewById(R.id.titleTV);
+        finishButton = (Button) tdRootView.findViewById(R.id.finBtn);
+        editButton = (Button) tdRootView.findViewById(R.id.editBtn);
+        deleteButton = (Button) tdRootView.findViewById(R.id.delBtn);
+
+        list.setOnItemClickListener(new ListItemClickListener());
+        finishButton.setOnClickListener(this);
+        editButton.setOnClickListener(this);
+        deleteButton.setOnClickListener(this);
 
         adapter =
                 new ArrayAdapter<String>(
@@ -40,5 +57,26 @@ public class ToDoList extends Fragment {
         list.setAdapter(adapter);
 
         return tdRootView;
+    }
+
+    private class ListItemClickListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+            getTitle = (String) adapterView.getItemAtPosition(position);
+            toDoTitle.setText(getTitle);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.finBtn:
+                break;
+            case R.id.editBtn:
+                break;
+            case R.id.delBtn:
+                break;
+        }
     }
 }
