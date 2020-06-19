62330fc885adfeba1f09d5a550d869c930c1e05f
Keisuke <ji.kei1220@gmail.com>
2020-06-20 01:33:36 +0900

    設定できる色を増やした

--- app/src/main/java/com/example/x3033076/finalextodocalendar/ColorSetDialogFragment.java
+++ app/src/main/java/com/example/x3033076/finalextodocalendar/ColorSetDialogFragment.java
@@ -13,7 +13,7 @@ import android.widget.Button;
 import androidx.fragment.app.DialogFragment;
 
 public class ColorSetDialogFragment extends DialogFragment implements View.OnClickListener {
-    Button setRed, setYellow, setLightGreen, setGreen, setLightBlue, setBlue;
+    Button setRed, setYellow, setLightYellow, setLightGreen, setGreen, setSkyBlue, setLightBlue, setBlue, setDeepPurple, setPurple;
     Resources resource;
 
     @Override
@@ -25,17 +25,25 @@ public class ColorSetDialogFragment extends DialogFragment implements View.OnCli
         View content = inflater.inflate(R.layout.color_set_layout, null);
         setRed = content.findViewById(R.id.setRedBtn);
         setYellow = content.findViewById(R.id.setYellowBtn);
+        setLightYellow = content.findViewById(R.id.setLightYellowBtn);
         setLightGreen = content.findViewById(R.id.setLightGreenBtn);
         setGreen = content.findViewById(R.id.setGreenBtn);
+        setSkyBlue = content.findViewById(R.id.setSlyBlueBtn);
         setLightBlue = content.findViewById(R.id.setLightBlueBtn);
         setBlue = content.findViewById(R.id.setBlueBtn);
+        setDeepPurple = content.findViewById(R.id.setDeepPurpleBtn);
+        setPurple = content.findViewById(R.id.setPurpleBtn);
 
         setRed.setOnClickListener(this);
         setYellow.setOnClickListener(this);
+        setLightYellow.setOnClickListener(this);
         setLightGreen.setOnClickListener(this);
         setGreen.setOnClickListener(this);
+        setSkyBlue.setOnClickListener(this);
         setLightBlue.setOnClickListener(this);
         setBlue.setOnClickListener(this);
+        setDeepPurple.setOnClickListener(this);
+        setPurple.setOnClickListener(this);
 
         resource = getResources();
         AddToDo.color = resource.getColor(R.color.colorLightBlue);
@@ -60,17 +68,29 @@ public class ColorSetDialogFragment extends DialogFragment implements View.OnCli
             case R.id.setYellowBtn:
                 setColor = resource.getColor(R.color.colorYellow);
                 break;
+            case R.id.setLightYellowBtn:
+                setColor = resource.getColor(R.color.colorLightYellow);
+                break;
             case R.id.setLightGreenBtn:
                 setColor = resource.getColor(R.color.colorLightGreen);
                 break;
             case R.id.setGreenBtn:
                 setColor = resource.getColor(R.color.colorGreen);
                 break;
-            case R.id.setBlueBtn:
-                setColor = resource.getColor(R.color.colorBlue);
+            case R.id.setSlyBlueBtn:
+                setColor = resource.getColor(R.color.colorSkyBlue);
                 break;
             default:
                 setColor = resource.getColor(R.color.colorLightBlue);
+                break;
+            case R.id.setBlueBtn:
+                setColor = resource.getColor(R.color.colorBlue);
+                break;
+            case R.id.setDeepPurpleBtn:
+                setColor = resource.getColor(R.color.colorDeepPurple);
+                break;
+            case R.id.setPurpleBtn:
+                setColor = resource.getColor(R.color.colorPurple);
                 break;
         }
         AddToDo.setColorButton.setBackgroundColor(setColor);
